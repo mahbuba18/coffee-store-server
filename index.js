b8ad05ca6bb4f32cab90383aa211ec3aeb4b8654
Mahbuba Islam Priya <mahbubacse18@gmail.com>
2025-03-03 16:41:15 +0600

    adding coffee

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app= express();
 const port=process.env.PORT||5000;
 
+//middleware
+app.use(cors());
+app.use(express.json());
+
 
 //coffee-master
 //ha6FSu5Vrik9ouur
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.whfiqab.mongodb.net/?appName=Cluster0`;
 // const uri = "mongodb+srv://coffee-master:ha6FSu5Vrik9ouur@cluster0.whfiqab.mongodb.net/?appName=Cluster0";
 
@@ -28,6 +29,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection=client.db('coffeeDB').collection('coffee')
+    
+    app.get('/coffee',async(req,res)=>{
+      const cursor=coffeeCollection.find();
+      const result=await cursor.toArray();
+      res.send(result);
+
+    })
+
+    app.post('/coffee', async(req,res)=>{
+      const newCoffee=req.body;
+      console.log(newCoffee);
+      const result= await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -38,9 +57,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-//middleware
-app.use(cors());
-app.use(express.json());
+
 
 
 
